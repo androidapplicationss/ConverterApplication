eec7eae3e4d23ef7458077cfd0c44e9478606d62
bhavesh.chand <bhavesh.chand@verizonconnect.com>
2024-07-31 14:17:53 +0530

    resolve conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,15 @@ plugins {
     id("androidx.room") version "$room_version" apply false
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Use the latest version of Gradle plugin compatible with Java 17 and Gradle version
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("androidx.room:room-gradle-plugin:2.6.1")
+        // Other classpath dependencies if necessary
+    }
+}
